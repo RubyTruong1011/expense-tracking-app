7f3c9721a02a6db09230951cbbe254f4e43c1a4f
Ruby Truong <rubytruong1011@gmail.com>
2022-10-16 23:20:21 -0400

    adding chart

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -5,7 +5,7 @@ import "./ChartBar.css";
 const ChartBar = (props) => {
   let barFillHeight = "0%";
 
-  if (props.max > 0) {
+  if (props.maxValue > 0) {
     barFillHeight = Math.round((props.value / props.maxValue) * 100) + "%";
   }
   return (
@@ -14,8 +14,9 @@ const ChartBar = (props) => {
         <div
           className="chart-bar__fill"
           style={{ height: barFillHeight }}></div>
-        <div className="chart-bar__label">{props.label}</div>
       </div>
+
+      <div className="chart-bar__label">{props.label}</div>
     </div>
   );
 };
